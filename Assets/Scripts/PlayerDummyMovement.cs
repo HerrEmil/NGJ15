050d12ef06054fc90c3e5e49e81241b4b54fad98
Christian Sunesen <christian_sunesen@hotmail.com>
2015-02-07 17:02:30 +0100

    Physics Programming

---
+++ Assets/Scripts/PlayerDummyMovement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDummyMovement : MonoBehaviour {
+
+	public float MovementSpeed = 1;
+	public GameObject StageCenter;
+	public float DesiredDistanceFromCenter;
+	public float GravityForce;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// Set the player object at the correct distance from the stage
+		Vector3 lookVectorNormal = GetNormalVectorToCenter();
+
+		Quaternion LookVector = Quaternion.LookRotation(lookVectorNormal);
+
+		transform.position = lookVectorNormal * (DesiredDistanceFromCenter-2);
+	}
+
+	// Gets normalized vector between this object and stage center
+	private Vector3 GetNormalVectorToCenter()
+	{
+		// Point the collider at the stage center
+		Vector3 CenterToColliderVector = this.transform.position - StageCenter.transform.position;
+		
+		Vector3 normalVector = CenterToColliderVector.normalized;
+
+		return normalVector;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		UpdateDistanceFromCenter();
+
+		Vector2 force = new Vector2(h,v);
+
+		this.rigidbody2D.AddForce(force * MovementSpeed);
+	}
+
+	private void UpdateDistanceFromCenter()
+	{
+		Vector2 vector2dToCenter = new Vector2(GetNormalVectorToCenter().x, GetNormalVectorToCenter().y);
+		rigidbody2D.AddForce(vector2dToCenter * GravityForce);
+
+	}
+}
---
+++ Assets/Scripts/PlayerGroundCollider.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerGroundCollider : MonoBehaviour {
+
+	public GameObject StageCenter;
+	public GameObject Player;
+	public float DesiredDistanceFromCenter;
+	public Vector3 CenterFacingVector = new Vector3(0,1,0);
+
+	private Quaternion RotationHolder = new Quaternion();
+
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// Get players position and update own position to match
+		if(Player)
+		{
+			Vector2 playerPosition = new Vector2(Player.transform.position.x, Player.transform.position.y);
+			Vector2 StageCenter2D = new Vector2(StageCenter.transform.position.x, StageCenter.transform.position.y);
+
+			Vector2 normalVector = (playerPosition-StageCenter2D).normalized;
+
+			this.transform.position = normalVector * DesiredDistanceFromCenter;
+		}
+
+		// Rotate to face stage center
+		if(StageCenter)
+		{
+			// Get vector between the collider and center point
+			Vector3 CenterToColliderVector = this.transform.position - StageCenter.transform.position;
+			Debug.Log (CenterToColliderVector);
+			// Normalize it
+			Vector3 normalVector = CenterToColliderVector.normalized;
+
+			RotationHolder.Set(normalVector.x,normalVector.y, normalVector.z, normalVector.z);
+
+			transform.rotation = RotationHolder;
+
+
+		}
+
+
+	}
+	
+}
