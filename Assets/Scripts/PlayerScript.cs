201852c1f0197cd8d3ab47f183b220eecfa26c07
lordjesus <danieljallov@gmail.com>
2015-02-07 23:49:37 +0100

    Added ball control

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,12 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class PlayerScript : MonoBehaviour {
 
     public int playerId = 0;
     public Color playerColor;
     public GameObject pointsText;
+
+    public int maxBalls = 3;
+    public List<GameObject> balls;
 
     private int playerPoints = 0;
 
@@ -20,5 +24,10 @@ public class PlayerScript : MonoBehaviour {
     {
         playerPoints -= points;
         pointsText.GetComponent<Text>().text = playerPoints.ToString();
+    }
+
+    public int GetNumberOfBalls()
+    {
+        return (balls != null) ? balls.Count : 0;
     }
 }
