6ddd7a837cfbcfc8c94922c81e726bb831e5e2d6
Christian Sunesen <christian_sunesen@hotmail.com>
2015-02-07 20:27:13 +0100

    Added Gamelogic object with Gamelogic script and SoundController script for sound effects and music

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameLogic : MonoBehaviour {
+
+	private static List<GameObject> Boxes;
+	private SoundController SoundController;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+		Boxes = new List<GameObject>();
+
+		SoundController = GetComponent<SoundController>();
+
+		SoundController.PlayMusicClip(0);
+
+		// Get all boxes at start
+		GameObject[] startBoxes = GameObject.FindGameObjectsWithTag("Boxes");
+
+		for(int i = 0; i < startBoxes.Length; i++)
+		{
+			// Adds all the start boxes to the more flexible Boxes array list
+			Boxes.Add(startBoxes[i]);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		UpdateLists();
+
+		UpdateMusic();
+	}
+
+	private void UpdateMusic()
+	{
+		if(GetBoxCount() < 50)
+		{
+			// Play more intense music
+			SoundController.StopMusicClip(0);
+			
+			SoundController.PlayMusicClip(1);
+		}
+		else if(GetBoxCount() < 20)
+		{
+			// Play even more intense music
+			SoundController.StopMusicClip(1);
+			
+			SoundController.PlayMusicClip(2);
+		}
+	}
+
+	public int GetBoxCount()
+	{
+		return Boxes.Count;
+	}
+
+	// Cleans out null objects from the lists when they've been destroyed
+	private void UpdateLists()
+	{
+		Boxes.RemoveAll(item => item == null);
+	}
+}
---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundController : MonoBehaviour {
+
+	//private GameLogic GameLogic;
+
+	public AudioSource[] MusicClips;
+	public AudioSource[] SoundClips;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//GameLogic = GetComponent<GameLogic>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	// Starts a specific music clip
+	public void PlayMusicClip(int clipNumber)
+	{
+		if(clipNumber <= MusicClips.Length)
+		{
+			MusicClips[clipNumber].Play();
+		}
+	}
+
+	// Stops a specific music clip
+	public void StopMusicClip(int clipNumber)
+	{
+		if(clipNumber <= MusicClips.Length)
+		{
+			MusicClips[clipNumber].Stop();
+		}
+	}
+
+	// Starts a specific sound clip
+	public void PlaySoundClip(int clipNumber)
+	{
+		if(clipNumber <= SoundClips.Length)
+		{
+			SoundClips[clipNumber].Play();
+		}
+	}
+
+	// Stops a specific sound clip
+	public void StopSoundClip(int clipNumber)
+	{
+		if(clipNumber <= SoundClips.Length)
+		{
+			SoundClips[clipNumber].Stop();
+		}
+	}
+
+	// Returns numbers of music clips
+	public int GetNumMusicClips()
+	{
+		return MusicClips.Length;
+	}
+
+	// Returns number of sound clips
+	public int GetNumSoundClips()
+	{
+		return SoundClips.Length;
+	}
+}
