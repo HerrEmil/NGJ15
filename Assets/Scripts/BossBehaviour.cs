d315bca091208dd5f037fdb85984317c2c8bc543
Hilindi <kwhjaltason@hotmail.com>
2015-02-07 23:33:47 +0100

    Created Boss Prefab

---
+++ Assets/Scripts/BossBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossBehaviour : MonoBehaviour
+{
+
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.transform.tag.Equals("Ball"))
+        {
+            bool hit = animator.GetBool("OnHit");
+            if (hit != true)
+            {
+                animator.SetBool("OnHit", true);
+                //Wait();
+            }
+        }
+    }
+
+    //IEnumerator Wait()
+    //{
+    //    yield return new WaitForSeconds(1);
+    //    animator.SetBool("OnHit", false);
+    //}
+}
