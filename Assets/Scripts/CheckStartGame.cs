6d4cbfd1fb0959f898412af4c5069d6750d4ef24
lordjesus <danieljallov@gmail.com>
2015-02-08 11:08:52 +0100

    Added intro level and build settings

---
+++ Assets/Scripts/CheckStartGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckStartGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ((Input.GetButton("P1_Fire") || Input.GetKey("up")) &&
+            (Input.GetButton("P2_Fire") || Input.GetKey("w")) &&
+            (Input.GetButton("P3_Fire") || Input.GetKey("b")) &&
+            (Input.GetButton("P4_Fire") || Input.GetKey("o")))
+        {
+            Application.LoadLevel("Level 1");
+        }
+	}
+}
